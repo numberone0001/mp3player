fe6c275c6d27a5e82cfca40350c44515f0946c32
Artur Yakovlev <artur.yakovlev1414@gmail.com>
2020-10-23 00:15:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/app/ui/main/Song.java
@@ -0,0 +1,61 @@
+package com.example.app.ui.main;
+
+import android.provider.MediaStore;
+
+public class Song {
+
+    private String name;
+    private String artist;
+    private String album;
+    private String duration;
+    private int number;
+    private String path;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
---
+++ app/src/main/java/com/example/app/ui/main/SongRetriever.java
@@ -0,0 +1,56 @@
+package com.example.app.ui.main;
+
+import android.media.MediaMetadataRetriever;
+import android.os.Environment;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class SongRetriever {
+    final String MEDIA_PATH = Environment.getExternalStorageDirectory() + "";
+    final int DEFAULT_SIZE = 20;
+
+    private ArrayList<String> getSongURLList(String path){
+        ArrayList<String> fileList = new ArrayList<String>(DEFAULT_SIZE);
+
+        File rootFolder = new File(path);
+        File[] files = rootFolder.listFiles();
+        if(files == null){
+            return fileList;
+        }
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                fileList.addAll(getSongURLList(file.getAbsolutePath()));
+            } else if (file.getName().endsWith(".mp3")) {
+                fileList.add(file.getAbsolutePath());
+            }
+        }
+        return fileList;
+    }
+
+    public ArrayList<Song> getSongList(){
+        ArrayList<String> songURLs = getSongURLList(MEDIA_PATH);
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+
+        ArrayList<Song> songList = new ArrayList<Song>(songURLs.size());
+        for(String url : songURLs){
+            retriever.setDataSource(url);
+            Song song = new Song();
+            song.setAlbum(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM));
+            song.setArtist(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUMARTIST));
+            song.setName(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE));
+            song.setDuration(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+            String number = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_CD_TRACK_NUMBER);
+            if(number != null){
+                song.setNumber(Integer.parseInt(number));
+            }
+            song.setPath(url);
+        }
+
+        return songList;
+    }
+
+
+
+}
